9b898dbf9633144bfb8f1aa605052f7fe1417e41
Ruchit Gevariya <ruchitgevariya@gmail.com>
2025-05-29 18:17:56 +0530

    change

--- backend/Cloudinary/config.js
+++ backend/Cloudinary/config.js
@@ -2,15 +2,14 @@ import dotenv from "dotenv";
 const result = dotenv.config();
 import {v2 as cloudinary} from "cloudinary"
 
+// cloudinary.config({
+//   cloud_name: process.env.CLOUD_NAME,
+//   api_key: process.env.API_KEY,
+//   api_secret: process.env.API_SECRET,
+// });
+
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET,
-});
-console.log("Cloudinary ENV:", {
-  CLOUD_NAME: process.env.CLOUD_NAME,
-  API_KEY: process.env.API_KEY,
-  API_SECRET: process.env.API_SECRET
-});
+  secure:true,
+})
 
 export default cloudinary
--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ import {fileURLToPath} from "url"
 import path from "path"
 import { log } from "console";
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3002','https://mega-zmovies.vercel.app'],  
+  origin: ['http://localhost:3000', 'http://localhost:3002','https://mega-zmovies.vercel.app','mega-zmovies-zols.vercel.app'],  
   credentials: true                
 }));
 
