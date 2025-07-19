74655c8b0c1429fe54603483b53e185357e31018
RuchitGevariya <gevariyaruchit@gmail.com>
2025-07-19 12:29:45 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ import {fileURLToPath} from "url"
 import path from "path"
 import { log } from "console";
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3002','https://mega-zmovies-zols.vercel.app','https://mega-zmovies.vercel.app/'],  
+  origin: ['http://localhost:3000', 'http://localhost:3002', 'https://mega-zmovies-zols.vercel.app', 'https://mega-zmovies.vercel.app'],  
   credentials: true                
 }));
 
