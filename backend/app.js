6b36ba3de94384ddbdaa0f3c53b778602fa6dca5
RuchitGevariya <gevariyaruchit@gmail.com>
2025-08-08 19:26:22 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ app.use(
     origin: [
       'http://localhost:3000',
       'http://localhost:3002',
-      'https://mega-zmovies-vyzg.vercel.app',
+      'https://mega-zmovies-zjum.vercel.app',
       'https://mega-zmovies-zols.vercel.app',
     ],
     credentials: true,
