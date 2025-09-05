f397376acc71499079ccc823e4cee3544dd70a6e
RuchitGevariya <gevariyaruchit@gmail.com>
2025-09-05 19:29:55 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -55,7 +55,7 @@ app.use("/api/admin",CheckAdmin, pictureRouter);
 app.use("/api/admin",CheckAdmin,bannerRouter)
 
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT||3001;
 app.listen(PORT,'0.0.0.0', () => {
   console.log(`server running on the http://localhost:${PORT}`);
   connect();
